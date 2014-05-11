a1a873268ec7d29d2f82b50a94421d2066170cc6
aporto <alex.itajuba@gmail.com>
2014-05-11 08:05:17 -0300

    first commit

---
+++ projects/guitar/build/obj/guitar_main.cpp.d
@@ -0,0 +1,17 @@
+d:\github\arduinonsense\projects\guitar\build\obj\guitar_main.cpp.o:  \
+ d:\github\arduinonsense\projects\guitar\source\guitar_main.cpp \
+  d:\github\arduinonsense\projects\guitar\source\/guitar_main.h \
+  D:\programas\developer\arduino\ide\arduino-1.0.5-r2\hardware\arduino\cores\arduino/arduino.h \
+  D:\programas\developer\arduino\ide\arduino-1.0.5-r2\hardware\arduino\cores\arduino/binary.h \
+  D:\programas\developer\arduino\ide\arduino-1.0.5-r2\hardware\arduino\cores\arduino/WCharacter.h \
+  D:\programas\developer\arduino\ide\arduino-1.0.5-r2\hardware\arduino\cores\arduino/WString.h \
+  D:\programas\developer\arduino\ide\arduino-1.0.5-r2\hardware\arduino\cores\arduino/HardwareSerial.h \
+  D:\programas\developer\arduino\ide\arduino-1.0.5-r2\hardware\arduino\cores\arduino/Stream.h \
+  D:\programas\developer\arduino\ide\arduino-1.0.5-r2\hardware\arduino\cores\arduino/Print.h \
+  D:\programas\developer\arduino\ide\arduino-1.0.5-r2\hardware\arduino\cores\arduino/Printable.h \
+  D:\programas\developer\arduino\ide\arduino-1.0.5-r2\hardware\arduino\cores\arduino/new.h \
+  D:\programas\developer\arduino\ide\arduino-1.0.5-r2\hardware\arduino\cores\arduino/USBAPI.h \
+  D:\programas\developer\arduino\ide\arduino-1.0.5-r2\hardware\arduino\variants\leonardo/pins_arduino.h \
+  d:\github\arduinonsense\projects\guitar\source/notes.h \
+  D:\projetos\eletronica\arduino\libs\Playstation2/PS2X_lib.h \
+  D:\programas\developer\arduino\ide\arduino-1.0.5-r2\hardware\arduino\cores\arduino/Arduino.h
---
+++ projects/guitar/source/guitar_main.h
@@ -0,0 +1,48 @@
+//-------------------------------------------------------------------
+#ifndef __guitar_main_H__
+#define __guitar_main_H__
+//-------------------------------------------------------------------
+ 
+#include <arduino.h>
+#include <notes.h>
+#include <PS2X_lib.h>
+ 
+//-------------------------------------------------------------------
+ 
+//-------------------------------------------------------------------
+ 
+#include <PS2X_lib.h>
+
+#define MAX_LEDS          4      // Maximum number of leds to play
+#define DELAY_PLAY_START  1000   // Initial delay during playtime
+#define DELAY_PLAY_MIN    100    // Minimum delay during playtime. Delay will decay down to this value
+#define DELAY_PLAY_DECAY  50     // Delay decreasing step.
+#define BUTTON_START      255
+#define BUTTON_1          0
+#define BUTTON_2          1 
+#define BUTTON_3          2
+#define BUTTON_4          3
+#define NO_BUTTON         -1
+
+#define PIN_SPEAKER      3
+#define PIN_LED_1        10
+#define PIN_LED_2        11
+#define PIN_LED_3        12
+#define PIN_LED_4        13
+
+#define GAMEPAD_CLK      7
+#define GAMEPAD_CMD      6
+#define GAMEPAD_ATT      5
+#define GAMEPAD_DAT      4
+
+#define MAX_SEQUENCE     256
+
+#define TONE_ERROR          956
+ 
+
+//-------------------------------------------------------------------
+#endif
+//-------------------------------------------------------------------
+ 
+ 
+ 
