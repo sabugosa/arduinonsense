d24b1c62fc37999373487113ca4d0dc96f0acb4a
Alex Porto <alex.itajuba@gmail.com>
2014-08-02 08:44:11 -0300

    adicionado o segue faixa do Rodrigo

---
+++ projetos/mariamole/robos/SegueFaixaRodrigo/source/seguefaixa.h
@@ -0,0 +1,15 @@
+/* File automatically created by MariaMole */ 
+#ifndef SEGUEFAIXA_H
+#define SEGUEFAIXA_H
+
+void leSensores();
+void aviso();
+void confAutomatica();
+void frente();
+void viraDireita();
+void viraEsquerda();
+void mensagem();
+void medianaSensores();
+
+
+#endif
